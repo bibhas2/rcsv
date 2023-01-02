c17f715c6394af4d9c03865870e89b33a640e7a4
bibhas <bibhas2@gmail.com>
2023-01-02 11:41:18 -0800

    Port to windows

--- src/mmap/file_mapper.rs
+++ src/mmap/file_mapper.rs
@@ -65,3 +65,89 @@ pub mod unix_map {
         }
     }
 }
+
+
+#[cfg(windows)]
+pub mod windows_map {
+    use std::{fs::File};
+    use std::os::windows::io::{AsRawHandle, RawHandle};
+    use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
+    use winapi::um::fileapi::{GetFileSize};
+    use winapi::um::memoryapi::{
+        CreateFileMappingW, FlushViewOfFile, MapViewOfFile, UnmapViewOfFile, VirtualProtect,
+        FILE_MAP_ALL_ACCESS, FILE_MAP_COPY, FILE_MAP_EXECUTE, FILE_MAP_READ, FILE_MAP_WRITE,
+    };
+    use winapi::um::winnt::PAGE_READONLY;
+
+    pub struct FileMapper {
+        file_size: usize,
+        map_handle: *mut winapi::ctypes::c_void,
+        ptr: *mut winapi::ctypes::c_void,
+        file: File,
+    }
+
+    impl FileMapper {
+        pub fn new(file_name: &str) -> Result<FileMapper, &str> {
+
+            let file = match File::open(file_name) {
+                Ok(f) => f,
+                Err(_) => return Err("Failed to open file in readonly mode.")
+            };
+    
+            unsafe {
+                let map_handle = CreateFileMappingW(
+                    file.as_raw_handle(),
+                    std::ptr::null_mut(),
+                    PAGE_READONLY,
+                    0,
+                    0,
+                    std::ptr::null(),
+                );
+
+                if map_handle == INVALID_HANDLE_VALUE {
+                    return Err("Failed to map file. CreateFileMappingW failed.");
+                }
+
+                let ptr = MapViewOfFile(
+                    map_handle,
+                    FILE_MAP_READ,
+                    0,
+                    0,
+                    0
+                );
+
+                if ptr == std::ptr::null_mut() {
+                    return Err("Failed to get a mapped view of file. MapViewOfFile() failed.");
+                }
+
+                let file_size = GetFileSize(file.as_raw_handle(), std::ptr::null_mut()) as usize;
+    
+                Ok(
+                    FileMapper {
+                        file_size,
+                        map_handle,
+                        ptr,
+                        file, 
+                    }
+                )
+            }
+        }
+
+        pub fn get_bytes(&self) -> &[u8] {
+            unsafe {std::slice::from_raw_parts(self.ptr as *const u8, self.file_size)}
+        }
+
+        pub fn size(&self) -> usize {
+            self.file_size
+        }
+    }
+
+    impl <'a> Drop for FileMapper {
+        fn drop(&mut self) {
+            unsafe {
+                UnmapViewOfFile(self.ptr);
+                CloseHandle(self.map_handle);
+            }
+        }
+    }
+}
--- src/mmap/mod.rs
+++ src/mmap/mod.rs
@@ -2,3 +2,6 @@ mod file_mapper;
 
 #[cfg(unix)]
 pub use file_mapper::unix_map::FileMapper;
+
+#[cfg(windows)]
+pub use file_mapper::windows_map::FileMapper;
